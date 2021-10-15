7cab4ad6c99f162ef6cbcfc77d6f543a62840dab
Sam Dannatt <SDannatt@qa.com>
2021-10-15 08:45:15 +0100

    git giving me issues

--- src/main/java/com/qa/wab/services/WabService.java
+++ src/main/java/com/qa/wab/services/WabService.java
@@ -1,7 +1,10 @@
 package com.qa.wab.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.qa.wab.data.Wab;
 import com.qa.wab.repo.WabRepo;
 
 @Service
@@ -15,4 +18,28 @@ public class WabService {
 
 	}
 
+	public Wab create(Wab newWab) {
+		return this.repo.save(newWab);
+	}
+
+	public Wab getById(Integer id) {
+		return this.repo.findById(id);
+
+	}
+
+	public List<Wab> getAll() {
+		return this.repo.findAll();
+	}
+
+	public Wab updateWab(Integer id, Wab newWab) {
+		Wab oldWab = this.repo.findById(id);
+		oldWab.setName(newWab.getName());
+		oldWab.setPointsValue(newWab.getPointsValue());
+
+	}
+
+	public boolean removeWab(Integer id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
 }
